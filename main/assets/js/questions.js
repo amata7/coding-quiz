b2e9f6902c286ac07fd12797bc18dc2ff94f9316
amata7 <adrianmata@live.com>
2021-03-23 23:52:32 -0700

    timer counts down and stops at 0, buttons take you to next question, object contain questions, answers, and options in separate js file

---
+++ main/assets/js/questions.js
@@ -0,0 +1,36 @@
+var questions = [
+    {
+        number:1,
+        question:'What does HTML stand for?',
+        answer: 'Hyper Text Markup Language',
+        option: [
+            'Hyper Text Markup Language',
+            'Hyper Tool Meta Language',
+            'How The Main Looks',
+            'Hyper Text Main Language'
+        ]
+    },
+    {
+        number:2,
+        question:'What does CSS stand for?',
+        answer: 'Cascading Style Sheets',
+        option: [
+            'Colorful Computer Styles',
+            'Creative Style Sheets',
+            'Colorful Computer Sheets',
+            'Cascading Style Sheets'
+        ]
+    },
+    {
+        number:3,
+        question:'What does JS stand for?',
+        answer: 'Java Script',
+        option: [
+            'Just Saying',
+            'Java Script',
+            'Journal Scripts',
+            'Java Styles'
+        ]
+    },
+    
+];
