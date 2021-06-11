8c4a96998b6c6b5928e3e6eb72b174a4a6425053
amata7 <adrianmata@live.com>
2021-06-10 21:06:12 -0700

    changed format of questions array in questions.js

--- main/js/questions.js
+++ main/js/questions.js
@@ -1,36 +1,32 @@
 var questions = [
-    {
-        number:1,
-        question:'What does HTML stand for?',
-        answer: 'Hyper Text Markup Language',
-        option: [
-            'Hyper Text Markup Language',
-            'Hyper Tool Meta Language',
-            'How The Main Looks',
-            'Hyper Text Main Language'
-        ]
+  {
+    question: "What does HTML stand for?",
+    correctAnswer: "A",
+    answers: {
+      A: "Hyper Text Markup Language",
+      B: "Hyper Tool Meta Language",
+      C: "How The Main Looks",
+      D: "Hyper Text Main Language",
     },
-    {
-        number:2,
-        question:'What does CSS stand for?',
-        answer: 'Cascading Style Sheets',
-        option: [
-            'Colorful Computer Styles',
-            'Creative Style Sheets',
-            'Colorful Computer Sheets',
-            'Cascading Style Sheets'
-        ]
+  },
+  {
+    question: "What does CSS stand for?",
+    correctAnswer: "D",
+    answers: {
+      A: "Colorful Computer Styles",
+      B: "Creative Style Sheets",
+      C: "Colorful Computer Sheets",
+      D: "Cascading Style Sheets",
     },
-    {
-        number:3,
-        question:'What does JS stand for?',
-        answer: 'Java Script',
-        option: [
-            'Just Saying',
-            'Java Script',
-            'Journal Scripts',
-            'Java Styles'
-        ]
+  },
+  {
+    question: "What does JS stand for?",
+    correctAnswer: "B",
+    answers: {
+      A: "Just Saying",
+      B: "Java Script",
+      C: "Journal Scripts",
+      D: "Java Styles",
     },
-    
+  },
 ];
