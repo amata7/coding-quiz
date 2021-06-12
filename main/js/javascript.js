7080a5488c0de2260af1f38e325b0a2f821dddc2
amata7 <adrianmata@live.com>
2021-06-11 23:19:51 -0700

    DRYed up the code, quiz is being build from an array of questions now

--- main/js/javascript.js
+++ main/js/javascript.js
@@ -2,39 +2,16 @@ var startBtn = document.getElementById("startBtn");
 var rules = document.getElementById("rules");
 var infoBox = document.getElementById("infoBox");
 var question1 = document.getElementById("question1");
-var question2 = document.getElementById("question2");
-var question3 = document.getElementById("question3");
 var results = document.getElementById("results");
 var submit1 = document.getElementById("submit1");
 var submit2 = document.getElementById("submit2");
 var submit3 = document.getElementById("submit3");
 var returnBtn = document.getElementById("returnBtn");
+const quizContainer = document.getElementById("quiz");
 
 startBtn.onclick = () => {
   infoBox.classList.add("hidden");
-  question1.classList.remove("hidden");
-  question1.classList.add("block");
-};
-
-submit1.onclick = () => {
-  question1.classList.remove("block");
-  question1.classList.add("hidden");
-  question2.classList.remove("hidden");
-  question2.classList.add("block");
-};
-
-submit2.onclick = () => {
-  question2.classList.remove("block");
-  question2.classList.add("hidden");
-  question3.classList.remove("hidden");
-  question3.classList.add("block");
-};
-
-submit3.onclick = () => {
-  question3.classList.remove("block");
-  question3.classList.add("hidden");
-  results.classList.remove("hidden");
-  results.classList.add("block");
+  buildQuiz();
 };
 
 returnBtn.onclick = () => {
@@ -43,3 +20,37 @@ returnBtn.onclick = () => {
   infoBox.classList.remove("hidden");
   infoBox.classList.add("block");
 };
+
+function buildQuiz() {
+  // variable to store the HTML output
+  const output = [];
+
+  // for each question...
+  questions.forEach((currentQuestion, questionNumber) => {
+    // variable to store the list of possible answers
+    const answers = [];
+
+    // and for each available answer...
+    for (letter in currentQuestion.answers) {
+      // ...add an HTML radio button
+      answers.push(
+        `<label class="block">
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+      );
+    }
+
+    // add this question and its answers to the output
+    output.push(
+      `<div class="question text-lg font-medium"> ${
+        currentQuestion.question
+      } </div>
+        <div class="answers"> ${answers.join("")} </div>`
+    );
+  });
+
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join("");
+}
